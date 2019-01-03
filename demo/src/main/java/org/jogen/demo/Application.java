e4f5e0c0ae747c7e0bbed41c8fc55d073b13f5fe
Jogen <jogen@tj-factoring.com>
2019-01-03 11:47:57 +0800

    add

---
+++ demo/src/main/java/org/jogen/demo/Application.java
@@ -0,0 +1,17 @@
+package org.jogen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ *
+ * @author Jogen
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
